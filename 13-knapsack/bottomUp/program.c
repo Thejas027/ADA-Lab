718509cd2af1b8f2c537afb6f8d07c99f2af53df
Thejas <teju270934@gmail.com>
2024-08-13 01:17:32 +0530

    added plotter function where manually need to generate the data to plot graph with diffrent test case

--- 13-knapsack/bottomUp/program.c
+++ 13-knapsack/bottomUp/program.c
@@ -3,7 +3,7 @@
 #define MAX 100
 
 int t[MAX][MAX], v[MAX], w[MAX], n, m;
-
+int count = 0;
 int max(int a, int b)
 {
 	return a > b ? a : b;
@@ -17,10 +17,14 @@ int knap(int m, int n)
 		{
 			if (i == 0 || j == 0)
 				t[i][j] = 0;
-			else if (j < w[i])
-				t[i][j] = t[i - 1][j];
 			else
-				t[i][j] = max(t[i - 1][j], v[i] + t[i - 1][j - w[i]]);
+			{
+				count++;
+				if (j < w[i])
+					t[i][j] = t[i - 1][j];
+				else
+					t[i][j] = max(t[i - 1][j], v[i] + t[i - 1][j - w[i]]);
+			}
 		}
 	}
 	return t[n][m];
@@ -58,12 +62,20 @@ void tester()
 		}
 	}
 	printf("\n");
+
+	printf("%d\t%d\n", n, count);
 }
 
-
+// to get the graph run the program for different test case atleast for 3 to 4 to generate the graph, so on testing with diffrent test case it would give the graph
+void plotter()
+{
+	FILE *f1 = fopen("knapsack.txt", "a");
+	tester();
+	fprintf(f1, "%d\t%d\n", n, count);
+}
 
 int main()
 {
-	tester();
+	plotter();
 	return 0;
 }
