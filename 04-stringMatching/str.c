e66ea4cdd2fd8bb6f7b81a3643c74546b1e6b086
Thejas <teju270934@gmail.com>
2024-08-14 22:55:28 +0530

    string matching using c is done only the tester need to implement the plotter function

---
+++ 04-stringMatching/str.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int count = 0;
+
+int stringMatching(char *text, char *pattern, int n, int m)
+{
+      count = 0;
+      for (int i = 0; i <= n - m; i++)
+      {
+            int j = 0;
+            while (j < m)
+            {
+                  count++;
+                  if (pattern[j] != text[i + j])
+                        break;
+                  j++;
+            }
+            if (j == m)
+            {
+                  printf("THE PATTERN FOUND \n");
+                  return count;
+            }
+      }
+      printf("THE PATTERN not found \n");
+      return count;
+}
+
+void tester()
+{
+      char text[100], pattern[100];
+
+      printf("Enter the text : ");
+      fgets(text, sizeof(text), stdin);
+      text[strcspn(text, "\n")] = '\0';
+
+      printf("Enter the pattern : ");
+      fgets(pattern, sizeof(pattern), stdin);
+      pattern[strcspn(pattern, "\n")] = '\0';
+
+      int n = strlen(text);
+      int m = strlen(pattern);
+
+      int result = stringMatching(text, pattern, n, m);
+      if (result == -1)
+            printf("pattern did not matched.\n");
+      else
+      {
+            printf("pattern matched.\n");
+            printf("Number of comparisons : %d", result);
+      }
+}
+
+int main()
+{
+      tester();
+      return 0;
+}
